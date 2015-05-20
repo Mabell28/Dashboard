411eb2d075f944c9b6d6e912b80f9e891fd35291
Mabell28 <mabellhuayanaytaype@gmail.com>
2015-05-20 17:08:38 -0500

    grid-function

---
+++ jquery.js
@@ -0,0 +1,19 @@
+$('.precio').click(function(){
+	//$('.advertencia').toggle();
+	$(this).parent().find('.ocultar').toggle();
+	$(this).toggle();
+});
+
+
+$('.precio').click(function(){
+	//$('.advertencia').toggle();
+	$(this).parent().find('.ocultar').toggle();
+	$(this).toggle();
+});
+
+
+/*$(document).ready(function(){
+	$('.ocultar').toggleClass();
+	$('.precio').show()
+}*/
+
